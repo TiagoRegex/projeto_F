e64b2da29334ab67785d11550c1bd42b07f4dd58
TiagoRegex <tiagolmpereira46@gmail.com>
2026-03-18 16:34:45 +0000

    change4

--- cabecalho/header.js
+++ cabecalho/header.js
@@ -1,8 +1,8 @@
-fetch("./header.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("header-container").innerHTML = html;
-  });
+// /fetch("./header.html")
+//   .then(res => res.text())
+//   .then(html => {
+//     document.getElementById("header-container").innerHTML = html;
+//   });
 
 
   /*=====================SOBRE====================================*/
